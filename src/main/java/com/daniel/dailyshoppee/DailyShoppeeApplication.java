006d22ab61e0b33f9d4d2196a4c2c53b4e2c6c42
Fredy Daniel <fredyd900@gmail.com>
2023-03-04 22:38:54 +0530

    project-initial-structure-created

---
+++ src/main/java/com/daniel/dailyshoppee/DailyShoppeeApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.dailyshoppee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyShoppeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyShoppeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/dailyshoppee/DailyShoppeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.dailyshoppee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyShoppeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
